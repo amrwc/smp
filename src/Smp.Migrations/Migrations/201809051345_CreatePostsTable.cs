e1fef16b293a659e4e840205fdb5ffec62e1a2e7
faibz <louiswrwright@outlook.com>
2018-09-05 16:17:28 +0100

    Initial database migrations.

---
+++ src/Smp.Migrations/Migrations/201809051345_CreatePostsTable.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using SimpleMigrations;
+
+namespace Smp.Migrations.Migrations
+{
+    [Migration(201809051345, "Create Posts table")]
+    public class CreatePostsTable : Migration
+    {
+        protected override void Up()
+        {
+            Execute(File.ReadAllText(@"..\..\..\Migrations\Up\201809051345_CreatePostsTable.sql"));
+        }
+
+        protected override void Down()
+        {
+            Execute(File.ReadAllText(@"..\..\..\Migrations\Down\201809051345_CreatePostsTable.sql"));
+        }
+    }
+}
