cdbba6ea3c4c0033266d5b5c908dee8d1af40ec5
faibz <louiswrwright@outlook.com>
2018-09-05 16:17:28 +0100

    Initial database migrations.

---
+++ src/Smp.Migrations/Migrations/201809050840_CreateUsersTable.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using SimpleMigrations;
+
+namespace Smp.Migrations.Migrations
+{
+    [Migration(201809050840, "Create Users table")]
+    public class CreateUsersTable : Migration
+    {
+        protected override void Up()
+        {
+            Execute(File.ReadAllText(@"..\..\..\Migrations\Up\201809050840_CreateUsersTable.sql"));
+        }
+
+        protected override void Down()
+        {
+            Execute(File.ReadAllText(@"..\..\..\Migrations\Down\201809050840_CreateUsersTable.sql"));
+        }
+    }
+}
