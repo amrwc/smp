fea8a08257e2345c2b83dde4dd64e128a2b1e97a
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2018-09-08 17:09:29 +0100

    Add tests for creating a user.

---
+++ src/Smp.Web/IDbConnectionFactory.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Smp.Web
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection GetDbConnection();
+    }
+
+    public class DbConnectionFactory : IDbConnectionFactory, IDisposable
+    {
+        private readonly IDbConnection _dbConnection;
+        public DbConnectionFactory(IConfiguration configuration)
+        {
+            _dbConnection = new SqlConnection(configuration.GetValue<string>("DatabaseConnectionString"));
+            _dbConnection.Open();
+        }
+
+        public void Dispose()
+            => _dbConnection?.Dispose();
+
+        public IDbConnection GetDbConnection() 
+            => _dbConnection;
+    }
+}
