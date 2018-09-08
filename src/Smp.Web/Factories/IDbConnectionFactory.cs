fcdc743eb49e85ed684d6d6edd6ea83643d042ab
faibz <louiswrwright@gmail.com>
2018-09-08 21:08:47 +0100

    Moved validation into validator class. Started validation of email and passwords.

--- src/Smp.Web/Factories/IDbConnectionFactory.cs
+++ src/Smp.Web/Factories/IDbConnectionFactory.cs
@@ -3,7 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
-namespace Smp.Web
+namespace Smp.Web.Factories
 {
     public interface IDbConnectionFactory
     {
