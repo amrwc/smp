3f2edf14b3e2e4263f678fbb9a8846430248b4c6
faibz <louiswrwright@gmail.com>
2018-09-10 18:21:24 +0100

    Fixed insert query.

--- src/Smp.Web/Repositories/UserRepository.cs
+++ src/Smp.Web/Repositories/UserRepository.cs
@@ -1,15 +1,14 @@
 ﻿using System.Data;
-using System;
 using System.Threading.Tasks;
 using Dapper;
 using Smp.Web.Factories;
-using Smp.Web.Models.DTOs;
+using Smp.Web.Models;
 
 namespace Smp.Web.Repositories
 {
     public interface IUserRepository
     {
-        Task CreateUser(Models.User newUser);
+        Task CreateUser(User newUser);
     }
 
     public class UserRepository : IUserRepository
@@ -21,12 +20,11 @@ namespace Smp.Web.Repositories
             _dbConnection = connectionFactory.GetDbConnection();
         }
 
-        // public async Task CreateUser(Models.User newUser) 
-        //     => await _dbConnection.InsertAsync(new User(newUser.Id, newUser.Username, newUser.Password, newUser.Email));
-
-        public async Task CreateUser(Models.User newUser) 
+        public async Task CreateUser(User newUser)
         {
-            await _dbConnection.ExecuteAsync("@INSERT INTO [dbo].[Users] ([Id], [Username], [Password], [Email]) VALUES (@Id, @Username, @Password, @Email)", new { Id = newUser.Id, Username = newUser.Username, Password = newUser.Password, Email = newUser.Email });
+            await _dbConnection.ExecuteAsync(
+                "INSERT INTO [dbo].[Users] ([Id], [Username], [Password], [Email]) VALUES (@Id, @Username, @Password, @Email)",
+                new {newUser.Id, newUser.Username, newUser.Password, newUser.Email});
         }
 
     }
