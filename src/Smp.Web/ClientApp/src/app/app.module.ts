914bd9f798de6ef5ff588417f0052691552049bd
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2018-09-11 16:17:47 +0100

    Initial Sign Up page styling. Add temporary background colour. Remove some unused content.

--- src/Smp.Web/ClientApp/src/app/app.module.ts
+++ src/Smp.Web/ClientApp/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
@@ -17,8 +16,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
-    SignUpComponent
+    SignUpComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -27,7 +25,6 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'sign-up', component: SignUpComponent},
     ])
   ],
