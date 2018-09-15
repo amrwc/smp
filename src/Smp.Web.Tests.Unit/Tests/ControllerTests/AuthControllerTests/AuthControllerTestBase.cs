c1ea04721e2f41c4f6fc00a06025a41441a17719
faibz <louiswrwright@gmail.com>
2018-09-15 15:13:53 +0100

    Stuff.

---
+++ src/Smp.Web.Tests.Unit/Tests/ControllerTests/AuthControllerTests/AuthControllerTestBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smp.Web.Tests.Unit.Tests.ControllerTests.AuthControllerTests
+{
+    public class AuthControllerTestBase
+    {
+        protected AuthController AuthController;
+
+        public void Setup()
+        {
+
+        }
+    }
+}
