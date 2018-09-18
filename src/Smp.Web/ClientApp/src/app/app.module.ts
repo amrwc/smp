0a324d98bc44559287067c349b1b56ce7d3460ac
amrwc <42574496+amrwc@users.noreply.github.com>
2018-09-18 19:05:12 +0100

    Further work on Homepage
    - take navbar and header out of the Home component and place them separately,
    - add navbar and header into the App component,
    - hide navbar and header on Sign in and Sign up pages.

--- src/Smp.Web/ClientApp/src/app/app.module.ts
+++ src/Smp.Web/ClientApp/src/app/app.module.ts
@@ -5,39 +5,55 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { AuthGuard } from './guards/auth.guard';
 import { AlreadySignedInGuard } from './guards/already-signed-in.guard';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { NavFooterComponent } from './nav-footer/nav-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     CounterComponent,
     SignUpComponent,
     SignInComponent,
+    NavHeaderComponent,
+    NavFooterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] },
-      { path: 'counter', component: CounterComponent, canActivate: [AuthGuard] },
-      { path: 'sign-up', component: SignUpComponent, canActivate: [AlreadySignedInGuard] },
-      { path: 'sign-in', component: SignInComponent, canActivate: [AlreadySignedInGuard] },
+      {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full',
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'counter',
+        component: CounterComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'sign-up',
+        component: SignUpComponent,
+        canActivate: [AlreadySignedInGuard]
+      },
+      {
+        path: 'sign-in',
+        component: SignInComponent,
+        canActivate: [AlreadySignedInGuard]
+      },
       { path: '**', redirectTo: '' }
     ])
   ],
-  providers: [
-    AuthGuard,
-    AlreadySignedInGuard
-  ],
+  providers: [AuthGuard, AlreadySignedInGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
