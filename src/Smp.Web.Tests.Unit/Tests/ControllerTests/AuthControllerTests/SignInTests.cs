bf50f9dc34ded5b0114782bdd697dad402831055
faibz <faibz@users.noreply.github.com>
2018-09-20 19:30:20 +0100

    Update SignInTests.cs

--- src/Smp.Web.Tests.Unit/Tests/ControllerTests/AuthControllerTests/SignInTests.cs
+++ src/Smp.Web.Tests.Unit/Tests/ControllerTests/AuthControllerTests/SignInTests.cs
@@ -42,24 +42,5 @@ namespace Smp.Web.Tests.Unit.Tests.ControllerTests.AuthControllerTests
             public void ThenAuthServiceVerifyUserShouldHaveBeenCalled()
                 => AuthService.Verify(service => service.VerifyUser(_signInRequest.Email, _signInRequest.Password), Times.Once);
         }
-
-//        [TestFixture]
-//        public class GivenInvalidLoginCredentials : AuthControllerTestBase
-//        {
-//            private const string Email = "InvalidEmail";
-//            private const string Password = "InvPass";
-//
-//            private IActionResult _result;
-//
-//            [OneTimeSetUp]
-//            public void WhenLoginGetsCalled()
-//            {
-//                _result = AuthController.Login(Email, Password);
-//            }
-//
-//            [Test]
-//            public void ThenResultShouldBeOkObjectResult()
-//                => Assert.IsInstanceOf<OkObjectResult>(_result);
-//        }
     }
 }
