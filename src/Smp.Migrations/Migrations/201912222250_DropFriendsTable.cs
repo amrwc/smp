4302235ceedde5b738f8dc370c8ef14392ddf1c0
faibz <louiswrwright@gmail.com>
2019-12-22 22:53:54 +0000

    Added final migrations.

---
+++ src/Smp.Migrations/Migrations/201912222250_DropFriendsTable.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using SimpleMigrations;
+
+namespace Smp.Migrations.Migrations
+{
+    [Migration(201912222250, "Drop Friends Table")]
+    public class DropFriendsTable : Migration
+    {
+        protected override void Up()
+        {
+            Execute(File.ReadAllText(@"..\..\..\Migrations\Up\201912222250_DropFriendsTable.sql"));
+        }
+
+        protected override void Down()
+        {
+            Execute(File.ReadAllText(@"..\..\..\Migrations\Down\201912222250_DropFriendsTable.sql"));
+        }
+    }
+}
