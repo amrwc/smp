cbb3cb675359e71a3a17ddb6962567a9c1d8f011
faibz <louiswrwright@gmail.com>
2019-12-22 21:54:05 +0000

    Upgraded to .NET Core 3.1

--- src/Smp.Web/Startup.cs
+++ src/Smp.Web/Startup.cs
@@ -2,10 +2,10 @@ using System;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Smp.Web.Factories;
 using Smp.Web.Repositories;
@@ -26,7 +26,7 @@ namespace Smp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(/*option => option.EnableEndpointRouting = false*/);
 
             services.AddSingleton<IDbConnectionFactory, DbConnectionFactory>();
             services.AddTransient<IUserRepository, UserRepository>();
@@ -58,7 +58,7 @@ namespace Smp.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -70,16 +70,17 @@ namespace Smp.Web
                 app.UseHsts();
             }
 
+            app.UseRouting(); // maybe
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
             app.UseAuthentication();
 
-            app.UseMvc(routes =>
+            //app.UseMvc();
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
 
             app.UseSpa(spa =>
