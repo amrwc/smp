c7ac1becfba4acb0c44582f5e57266caee6babbe
faibz <louiswrwright@gmail.com>
2019-12-27 20:54:50 +0000

    Added some attributes to request models for ez validation.

--- src/Smp.Web/Models/Requests/CreateUserRequest.cs
+++ src/Smp.Web/Models/Requests/CreateUserRequest.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Smp.Web.Models.Requests
 {
     public class CreateUserRequest
     {
+        [Required]
         public string FullName { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }
