94db7c41d96b98a8df5b7be7fe637d71cfb4946f
faibz <louiswrwright@gmail.com>
2019-12-29 12:53:35 +0000

    Added functionality towards issue #24. Added completed column to create actions table (yolo not in a separate migration). Renamed method on accounts service. Added actions repository to handle inserting into actions table.

--- src/Smp.Web/Models/DTOs/Action.cs
+++ src/Smp.Web/Models/DTOs/Action.cs
@@ -7,6 +7,7 @@ namespace Smp.Web.Models.DTOs
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public byte ActionTypeId { get; set; }
+        public bool Completed { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ExpiresAt { get; set; }
 
---
+++ src/Smp.Web/Repositories/ActionsRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Smp.Web.Factories;
+using Smp.Web.Models;
+
+namespace Smp.Web.Repositories
+{
+    public interface IActionsRepository
+    {
+        Task CreateAction(Models.Action action);
+    }
+
+    public class ActionsRepository : IActionsRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public ActionsRepository(IDbConnectionFactory connectionFactory)
+        {
+            _dbConnection = connectionFactory.GetDbConnection();
+        }
+
+        public async Task CreateAction(Models.Action action)
+        {
+            await _dbConnection.ExecuteAsync(
+                "INSERT INTO [Actions] ([Id], [UserId], [ActionTypeId], [Completed], [CreatedAt], [ExpiresAt]) VALUES (@Id, @UserId, @ActionTypeId, @Completed, @CreatedAt, @ExpiresAt)",
+                new { Id = action.Id, UserId = action.UserId, ActionTypeId = (byte) action.ActionType, Completed = action.Completed, CreatedAt = action.CreatedAt, ExpiresAt = action.ExpiresAt });
+        }
+    }
+}
