d7670293bffae0bf23fc19b7b91b04742058feee
faibz <louiswrwright@gmail.com>
2019-12-29 21:11:29 +0000

    Added most client changes for starting the password reset process. Still need to add the success message for when a password reset has been started.

---
+++ src/Smp.Web/ClientApp/src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from '../services/accounts.service';
+import { debug } from 'util';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss']
+})
+export class ForgotPasswordComponent implements OnInit {
+
+  public accountEmail: string;
+  public loading: boolean;
+
+  public startPasswordResetUnsuccessful: boolean = false;
+  public validationError: Error;
+  public response: string;
+
+  constructor(private accountsService: AccountsService) { }
+
+  ngOnInit() {
+  }
+
+  public startPasswordReset(): void {
+    this.validationError = null;
+    this.startPasswordResetUnsuccessful = false;
+    this.accountEmail = this.accountEmail.toLowerCase();
+
+    this.accountsService.forgottenPassword(this.accountEmail).subscribe(
+      () => {
+        this.loading = false;
+        this.response = "An email has been sent to you. Click it to reset your password!";
+      },
+      error => {
+        debugger;
+        this.loading = false;
+        this.startPasswordResetUnsuccessful = true;
+        this.validationError = error.error;
+      }
+    )
+  }
+
+}
