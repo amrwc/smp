472b43108ed4134d6dde9d69368d3d4f10bcb7f2
faibz <louiswrwright@gmail.com>
2020-01-11 23:30:39 +0000

    #27 - Added button to add friends. Button will be disabled after the friend request has been sent and until the request has been responded to. This will need to be updated when users can add friends to add another condition.

---
+++ src/Smp.Web/ClientApp/src/app/services/requests.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CreateRequestRequest } from '../models/requests/create-request-request';
+import { RequestType } from '../models/request-type.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestsService {
+
+  private httpHeaders = new HttpHeaders();
+
+  constructor(private httpClient: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    let authToken = "Bearer " + JSON.parse(localStorage.getItem('currentUser')).token;
+    this.httpHeaders = this.httpHeaders.set('Authorization', authToken);
+  }
+
+  public sendRequest(req: CreateRequestRequest): Observable<Object> {
+    return this.httpClient.post(this.baseUrl + "api/Requests/SendRequest/", req, { headers: this.httpHeaders });
+  }
+
+  public getRequest(senderId: string, receiverId: string, reqType: RequestType): Observable<Request> {
+    return this.httpClient.get<Request>(`${this.baseUrl}api/Requests/GetRequest/${senderId}/${<number>reqType}/${receiverId}`, { headers: this.httpHeaders })
+  }
+}
