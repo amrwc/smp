215f194d091f98453f1d9d6c16dcc1a9c90b5808
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2020-02-20 22:16:04 +0000

    Add `CreateConversationTests`

--- src/Smp.Web.Tests.Unit/Tests/ControllerTests/ConversationsControllerTests/ConversationsControllerTestBase.cs
+++ src/Smp.Web.Tests.Unit/Tests/ControllerTests/ConversationsControllerTests/ConversationsControllerTestBase.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Smp.Web.Controllers;
