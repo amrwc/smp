c4a2d4212b7de4c0e3df26eaf4a4afaec9b176ab
faibz <louiswrwright@gmail.com>
2020-03-03 19:25:19 +0000

    #79 - Started solution. Created util method for migration classes to use.

---
+++ src/Smp.Migrations/MigrationUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Smp.Migrations
+{
+    internal class MigrationUtils
+    {
+        public static string ReadMigration(string migrationName, bool upMigration)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using var stream = assembly.GetManifestResourceStream($"Smp.Migrations.Migrations.{(upMigration ? "Up" : "Down" )}.{migrationName}.sql");
+
+            if (stream == null) throw new Exception("Embedded resource could not be found.");
+
+            return new StreamReader(stream).ReadToEnd();
+        }
+    }
+}
