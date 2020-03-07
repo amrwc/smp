ea0a08f1e94f46fbc34dfe917db6e9a590b016ca
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2020-03-07 22:41:22 +0000

    Add tests for Relationships, Requests, and Users services

--- src/Smp.Web/ClientApp/src/app/services/users.service.spec.ts
+++ src/Smp.Web/ClientApp/src/app/services/users.service.spec.ts
@@ -1,16 +1,66 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { CreateUserRequest } from '../models/requests/create-user-request';
 import { UsersService } from './users.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UsersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-    providers: [{ provide: 'BASE_URL', useValue: "https://www.smp.org/" }]
-  }));
+  const baseUrl = 'https://www.smp.org/';
+  let httpClient: HttpClient;
+  let httpClientGetSpy: jasmine.Spy;
+  let httpClientPostSpy: jasmine.Spy;
+  let service: UsersService;
 
-  it('should be created', () => {
-    const service: UsersService = TestBed.get(UsersService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: 'BASE_URL', useValue: baseUrl }],
+    });
+    httpClient = TestBed.get(HttpClient);
+    service = TestBed.get(UsersService);
+    httpClientGetSpy = spyOn(httpClient, 'get');
+    httpClientPostSpy = spyOn(httpClient, 'post');
+  });
+
+  describe('createUser', () => {
+    const createUserReq = {
+      fullName: 'John Doe',
+      password: 'lk1jn43kl1nj34###XXCC',
+      confirmPassword: 'lk1jn43kl1nj34###XXCC',
+      email: 'my@email.com',
+    } as CreateUserRequest;
+
+    it('should have called HttpClient.post correctly', () => {
+      service.createUser(createUserReq);
+      expect(httpClientPostSpy.calls.count()).toEqual(1);
+      expect(httpClientPostSpy.calls.argsFor(0)).toEqual([
+        `${baseUrl}api/Users/CreateUser/`,
+        createUserReq,
+      ]);
+    });
+  });
+
+  describe('getUser', () => {
+    const userId = 'aksdjn-123kwqjen-cxnzkj';
+    beforeAll(() => {
+      localStorage.setItem('currentUser', JSON.stringify({ token: 'n21k32n3j' }));
+    });
+    afterAll(() => {
+      localStorage.removeItem('currentUser');
+    });
+
+    it('should have called HttpClient.get correctly', () => {
+      const headers = new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${JSON.parse(localStorage.getItem('currentUser')).token}`
+      );
+      service.getUser(userId);
+      expect(httpClientGetSpy.calls.count()).toEqual(1);
+      expect(httpClientGetSpy.calls.argsFor(0)).toEqual([
+        `${baseUrl}api/Users/GetUser/${userId}`,
+        { headers },
+      ]);
+    });
   });
 });
