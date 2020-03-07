768465da9dc733ffc8a3fd597a04641405ce2ab4
amrwc <42574496+amrwc@users.noreply.github.com>
2020-03-07 23:39:43 +0000

    #50: Add return value expectations to Relationships service

--- src/Smp.Web/ClientApp/src/app/services/relationships.service.spec.ts
+++ src/Smp.Web/ClientApp/src/app/services/relationships.service.spec.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { of } from 'rxjs';
+import { Relationship } from '../models/relationship';
 import { RelationshipType } from '../models/relationship-type.enum';
 import { RelationshipsService } from './relationships.service';
 
@@ -11,7 +13,6 @@ describe('RelationshipsService', () => {
   const userTwoId = 'adsifasfuh1231231-asxzcz9v8bc9-213123';
   const relationshipType = RelationshipType.Friend;
   let headers: Object;
-  let httpClient: HttpClient;
   let httpClientGetSpy: jasmine.Spy;
   let service: RelationshipsService;
 
@@ -31,8 +32,6 @@ describe('RelationshipsService', () => {
       providers: [{ provide: 'BASE_URL', useValue: baseUrl }],
     });
     service = TestBed.get(RelationshipsService);
-    httpClient = TestBed.get(HttpClient);
-    httpClientGetSpy = spyOn(httpClient, 'get');
   });
 
   afterAll(() => {
@@ -40,6 +39,17 @@ describe('RelationshipsService', () => {
   });
 
   describe('getRelationship', () => {
+    const expected: Relationship = {
+      userOneId,
+      userTwoId,
+      relationshipType,
+      createdAt: new Date(),
+    } as Relationship;
+
+    beforeEach(() => {
+      httpClientGetSpy = spyOn(TestBed.get(HttpClient), 'get').and.returnValue(of(expected));
+    });
+
     it('should have called HttpClient.get correctly', () => {
       service.getRelationship(userOneId, userTwoId, relationshipType);
       expect(httpClientGetSpy.calls.count()).toEqual(1);
@@ -48,9 +58,37 @@ describe('RelationshipsService', () => {
         headers,
       ]);
     });
+
+    it('should have returned the expected values', () => {
+      const result = service.getRelationship(userOneId, userTwoId, relationshipType);
+      result.subscribe({
+        next: (relationship: Relationship) => {
+          expect(relationship).toEqual(expected);
+        },
+      });
+    });
   });
 
   describe('getRelationships', () => {
+    const expected: Relationship[] = [
+      {
+        userOneId,
+        userTwoId,
+        relationshipType,
+        createdAt: new Date(),
+      },
+      {
+        userOneId,
+        userTwoId,
+        relationshipType,
+        createdAt: new Date(),
+      },
+    ] as Relationship[];
+
+    beforeEach(() => {
+      httpClientGetSpy = spyOn(TestBed.get(HttpClient), 'get').and.returnValue(of());
+    });
+
     it('should have called HttpClient.get correctly', () => {
       service.getRelationships(userOneId, relationshipType);
       expect(httpClientGetSpy.calls.count()).toEqual(1);
@@ -58,6 +96,15 @@ describe('RelationshipsService', () => {
         `${baseUrl}api/Relationships/GetRelationships/${userOneId}/${relationshipType}`,
         headers,
       ]);
+    });
+
+    it('should have returned the expected values', () => {
+      const result = service.getRelationships(userOneId, relationshipType);
+      result.subscribe({
+        next: (relationships: Relationship[]) => {
+          expect(relationships).toEqual(expected);
+        },
+      });
     });
   });
 });
