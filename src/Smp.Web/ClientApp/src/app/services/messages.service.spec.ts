ffa6d05baaa499c4fe64164755bb2057ffce3f15
faibz <louiswrwright@gmail.com>
2020-03-07 22:42:04 +0000

    #50 - added tests for conversations, messages, and posts services.

--- src/Smp.Web/ClientApp/src/app/services/messages.service.spec.ts
+++ src/Smp.Web/ClientApp/src/app/services/messages.service.spec.ts
@@ -2,15 +2,100 @@ import { TestBed } from '@angular/core/testing';
 
 import { MessagesService } from './messages.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CreateMessageRequest } from '../models/requests/create-message-request';
+import { of } from 'rxjs';
+import { Message, FriendlyMessage } from '../models/message';
 
 describe('MessagesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-    providers: [{ provide: 'BASE_URL', useValue: "https://www.smp.org/" }]
-  }));
+  const baseUrl = 'https://www.smp.org/';
 
-  it('should be created', () => {
-    const service: MessagesService = TestBed.get(MessagesService);
-    expect(service).toBeTruthy();
+  let httpClient: HttpClient;
+  let service: MessagesService;
+
+  let httpClientGetSpy: jasmine.Spy;
+  let httpClientPostSpy: jasmine.Spy;
+
+  const authHeaders = new HttpHeaders().set('Authorization', 'Bearer token');
+
+  beforeAll(() => {
+    localStorage.setItem('currentUser', '{ "id": "id", "token": "token" }');
+  });
+
+  afterAll(() => {
+    localStorage.removeItem('currentUser');
+  })
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: 'BASE_URL', useValue: "https://www.smp.org/" }]
+    });
+
+    service = TestBed.get(MessagesService);
+
+    httpClientPostSpy = spyOn(TestBed.get(HttpClient), 'post');
+});
+
+  describe('getMessagesFromConversation', () => {
+    beforeEach(() => {
+      httpClientGetSpy = spyOn(TestBed.get(HttpClient), 'get')
+        .and.returnValue(of(expectedMessages));
+    });
+
+    const conversationId = 'conversationId';
+    const expectedMessages: Message[] = [
+      new Message({
+        id: 1,
+        senderId: 'senderId-1',
+        createdAt: new Date(),
+        content: 'content-1',
+        conversationId: conversationId
+      } as Message),
+      new Message({
+        id: 2,
+        senderId: 'senderId-2',
+        createdAt: new Date(),
+        content: 'content-2',
+        conversationId: conversationId
+      } as Message)
+    ];
+
+    it('shouild have returned the expected value', () => {
+      const messagesObserv = service.getMessagesFromConversation(conversationId);
+
+      messagesObserv.subscribe({
+        next: (messages: FriendlyMessage[]) => {
+          expect(messages).toEqual(expectedMessages.map(msg => new FriendlyMessage(msg)));
+        }
+      });
+    });
+
+    it('should have called HttpClient get correctly', () => {
+      service.getMessagesFromConversation(conversationId);
+
+      expect(httpClientGetSpy.calls.count()).toEqual(1);
+      expect(httpClientGetSpy.calls.argsFor(0)).toEqual([
+        `${baseUrl}api/Messages/GetMessagesFromConversation/${conversationId}?count=10&page=0`,
+        { headers: authHeaders }
+      ]);
+    });
+  });
+
+  describe('createMessage', () => {
+    const msgReq = {
+      senderId: 'senderId'
+    } as CreateMessageRequest;
+
+    it('should have called HttpClient post correctly', () => {
+      service.createMessage(msgReq);
+
+      expect(httpClientPostSpy.calls.count()).toEqual(1);
+      expect(httpClientPostSpy.calls.argsFor(0)).toEqual([
+        `${baseUrl}api/Messages/CreateMessage`,
+        msgReq,
+        { headers: authHeaders }
+      ]);
+    });
   });
 });
--- src/Smp.Web/ClientApp/src/app/services/posts.service.spec.ts
+++ src/Smp.Web/ClientApp/src/app/services/posts.service.spec.ts
@@ -2,15 +2,87 @@ import { TestBed } from '@angular/core/testing';
 
 import { PostsService } from './posts.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { CreatePostRequest } from '../models/requests/create-post-request';
+import { of } from 'rxjs';
+import { Post } from '../models/post';
 
 describe('PostsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-    providers: [{ provide: 'BASE_URL', useValue: "https://www.smp.org/" }]
-  }));
+  const baseUrl = 'https://www.smp.org/';
 
-  it('should be created', () => {
-    const service: PostsService = TestBed.get(PostsService);
-    expect(service).toBeTruthy();
+  let service: PostsService;
+
+  let httpClientGetSpy: jasmine.Spy;
+  let httpClientPostSpy: jasmine.Spy;
+
+  const authHeaders = new HttpHeaders().set('Authorization', 'Bearer token');
+
+  beforeAll(() => {
+    localStorage.setItem('currentUser', '{ "id": "id", "token": "token" }');
+  });
+
+  afterAll(() => {
+    localStorage.removeItem('currentUser');
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: 'BASE_URL', useValue: "https://www.smp.org/" }]
+    });
+
+    httpClientPostSpy = spyOn(TestBed.get(HttpClient), 'post');
+
+    service = TestBed.get(PostsService);
+  });
+
+  describe('createPost', () => {
+    const postReq = {
+      senderId: 'senderId'
+    } as CreatePostRequest;
+
+    it('should have called HttpClient post correctly', () => {
+      service.createPost(postReq);
+
+      expect(httpClientPostSpy.calls.count()).toEqual(1);
+      expect(httpClientPostSpy.calls.argsFor(0)).toEqual([
+        `${baseUrl}api/Posts/CreatePost`,
+        postReq,
+        { headers: authHeaders }
+      ]);
+    });
+  });
+
+  describe('getPosts', () => {
+    beforeEach(() => {
+      httpClientGetSpy = spyOn(TestBed.get(HttpClient), 'get')
+        .and.returnValue(of(expectedPosts));
+    });
+
+    const receiverId = 'receiverId';
+    const expectedPosts: Post[] = [
+      { content: 'content-1' } as Post,
+      { content: 'content-2' } as Post
+    ]
+
+    it('should have returned the expected value', () => {
+      const postsObserv = service.getPosts(receiverId);
+
+      postsObserv.subscribe({
+        next: ((posts: Post[]) => {
+          expect(posts).toEqual(expectedPosts);
+        })
+      });
+    });
+
+    it('should have called HttpClient get correctly', () => {
+      service.getPosts(receiverId);
+
+      expect(httpClientGetSpy.calls.count()).toEqual(1);
+      expect(httpClientGetSpy.calls.argsFor(0)).toEqual([
+        `${baseUrl}api/Posts/GetPosts/${receiverId}`,
+        { headers: authHeaders }
+      ]);
+    });
   });
 });
