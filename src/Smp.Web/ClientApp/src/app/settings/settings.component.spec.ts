612a499c0517d70042f8f84b5c285cdfa7e7b4cb
amrwc <42574496+amrwc@users.noreply.github.com>
2020-03-08 23:22:01 +0000

    #50: Add tests for Settings component

--- src/Smp.Web/ClientApp/src/app/settings/settings.component.spec.ts
+++ src/Smp.Web/ClientApp/src/app/settings/settings.component.spec.ts
@@ -1,22 +1,31 @@
-import { TestBed, ComponentFixture } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { SettingsComponent } from "./settings.component";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe("SettingsComponent", () => {
+import { SettingsComponent } from './settings.component';
+
+describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [SettingsComponent],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule],
     }).compileComponents();
     fixture = TestBed.createComponent(SettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
+  describe('signOut', () => {
+    it('should navigate to /sign-in', () => {
+      const routerNavigateSpy: jasmine.Spy = spyOn(TestBed.get(Router), 'navigate');
+      localStorage.setItem('currentUser', JSON.stringify({ currentUser: 'bob' }));
+      component.signOut();
+      expect(localStorage.getItem('currentUser')).toBeNull();
+      expect(routerNavigateSpy.calls.count()).toEqual(1);
+      expect(routerNavigateSpy.calls.argsFor(0)).toEqual([['/sign-in']]);
+    });
   });
 });
