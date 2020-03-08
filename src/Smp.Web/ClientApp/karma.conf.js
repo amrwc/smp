341078de87488c8e956e640f6a1b9547b3477c3a
faibz <louiswrwright@gmail.com>
2020-03-08 11:12:48 +0000

    #50 - chrome headless config.

--- src/Smp.Web/ClientApp/karma.conf.js
+++ src/Smp.Web/ClientApp/karma.conf.js
@@ -25,10 +25,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome'],
     singleRun: false,
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
+      ChromeHeadless: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
