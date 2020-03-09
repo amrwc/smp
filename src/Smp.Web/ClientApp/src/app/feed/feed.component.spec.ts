79e00f90762c35feb2985a64e1412df35f96d9ca
faibz <louiswrwright@gmail.com>
2020-03-09 20:44:14 +0000

    #50 - Added tests to feed component.

--- src/Smp.Web/ClientApp/src/app/feed/feed.component.spec.ts
+++ src/Smp.Web/ClientApp/src/app/feed/feed.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FeedComponent } from './feed.component';
 import { PostsService } from '../services/posts.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { Post } from '../models/post';
 
 describe('FeedComponent', () => {
   let component: FeedComponent;
@@ -20,7 +22,40 @@ describe('FeedComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('getPosts', () => {
+    let postsServiceGetPostsSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      postsServiceGetPostsSpy = spyOn(TestBed.inject(PostsService), 'getPosts');
+    });
+
+    describe('when there is no receiver id', () => {
+      it('PostsService getPosts should not get called', () => {
+        expect(postsServiceGetPostsSpy.calls.count()).toEqual(0);
+      });
+    });
+
+    describe('when there is a receiver id', () => {
+      const posts = [ { id: 'postid' } as Post ];
+
+      beforeEach(() => {
+        component.receiverId = 'receiverId';
+
+        postsServiceGetPostsSpy.and.returnValue(of(posts));
+      });
+
+      it('PostsService getPosts should get called', () => {
+        component.getPosts();
+
+        expect(postsServiceGetPostsSpy.calls.count()).toEqual(1);
+        expect(postsServiceGetPostsSpy.calls.argsFor(0)).toEqual(['receiverId']);
+      });
+
+      it('posts should be as expected', () => {
+        component.getPosts();
+
+        expect(component.posts).toEqual(posts);
+      });
+    })
   });
 });
