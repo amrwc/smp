0ef5b4156aa8893cfadeea06b9795a05cab3ce7b
faibz <louiswrwright@gmail.com>
2020-03-10 22:37:16 +0000

    #50 - Added tests for message-composer.

--- src/Smp.Web/ClientApp/src/app/message-composer/message-composer.component.spec.ts
+++ src/Smp.Web/ClientApp/src/app/message-composer/message-composer.component.spec.ts
@@ -9,16 +9,22 @@ import { ConversationsService } from '../services/conversations.service';
 import { UsersService } from '../services/users.service';
 import { GlobalHelper } from '../helpers/global';
 import { AngularMaterialModule } from '../angular-material.module';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { Relationship } from '../models/relationship';
 import { RelationshipType } from '../models/relationship-type.enum';
 import { User } from '../models/user';
+import { CreateConversationRequest } from '../models/requests/create-conversation-request';
 
 describe('MessageComposerComponent', () => {
   let component: MessageComposerComponent;
   let fixture: ComponentFixture<MessageComposerComponent>;
 
   const userId = 'userid-1'
+
+  const friends = [
+    { id: 'userid-1', fullName: 'Jack Ryan' } as User,
+    { id: 'userid-2', fullName: 'Tom Cruise' } as User
+  ];
 
   beforeEach(() => {
     localStorage.setItem('currentUser', JSON.stringify({ id: userId }));
@@ -42,6 +48,122 @@ describe('MessageComposerComponent', () => {
 
   afterEach(() => {
     localStorage.removeItem('currentUser');
+  });
+
+  describe('filterFriends', () => {
+    beforeEach(() => {
+      component.filteredFriends = null;
+      component.friends = friends;
+    });
+
+    it('should set filteredFriends correctly', () => {
+      component.filterFriends(' Rya');
+      expect(component.filteredFriends).toEqual([friends[0]]);
+
+      component.filterFriends('om C');
+      expect(component.filteredFriends).toEqual([friends[1]]);
+    });
+  });
+
+  describe('displayFriend', () => {
+    beforeEach(() => {
+      component.friends = friends;
+    });
+
+    it('should return the expected value', () => {
+      const friend = component.displayFriend('userid-2');
+      expect(friend).toEqual('Tom Cruise');
+
+      const nonFriend = component.displayFriend('userid-3');
+      expect(nonFriend).toEqual('Unknown');
+    });
+  });
+
+  describe('sendMessage', () => {
+    let cnvSvcCreateConversationSpy: jasmine.Spy;
+    let conversationReadyEmitSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      cnvSvcCreateConversationSpy = spyOn(TestBed.inject(ConversationsService), 'createConversation')
+      conversationReadyEmitSpy = spyOn(component.conversationReady, 'emit');
+      component.friends = friends;
+    });
+
+    describe('if the receiver is not a friend', () => {
+      beforeEach(() => {
+        component.createConversationRequest.receiverId = 'userid-3';
+      });
+
+      it('should not call ConversationsService createConversation', () => {
+        component.sendMessage();
+
+        expect(cnvSvcCreateConversationSpy.calls.count()).toEqual(0);
+      });
+    });
+
+    describe('if ConversationsService createConversation returns a conflict error', () => {
+      let convReq = new CreateConversationRequest();
+
+      beforeEach(() => {
+        component.createConversationRequest.receiverId = friends[1].id;
+        cnvSvcCreateConversationSpy
+          .and.returnValue(throwError({ status: 409, error: 'conversationid-error' }));
+        convReq.receiverId = friends[1].id;
+        convReq.senderId = userId;
+      });
+
+      it('should call ConversationsService createConversation correctly', () => {
+        component.sendMessage();
+
+        expect(cnvSvcCreateConversationSpy.calls.count()).toEqual(1);
+        expect(cnvSvcCreateConversationSpy.calls.argsFor(0)).toEqual([convReq]);
+      });
+
+      it('should emit a conversationReady event', () => {
+        component.sendMessage();
+
+        expect(conversationReadyEmitSpy.calls.count()).toEqual(1);
+        expect(conversationReadyEmitSpy.calls.argsFor(0)).toEqual(['conversationid-error']);
+      });
+
+      it('should have set variables correctly', () => {
+        component.sendMessage();
+
+        expect(component.loading).toEqual(false);
+      });
+    });
+
+    describe('if it completes successfully', () => {
+      let convReq = new CreateConversationRequest();
+
+      beforeEach(() => {
+        component.createConversationRequest.receiverId = friends[1].id;
+        cnvSvcCreateConversationSpy
+          .and.returnValue(of('conversationid-1'));
+        convReq.receiverId = friends[1].id;
+        convReq.senderId = userId;
+      });
+
+      it('should call ConversationsService createConversation correctly', () => {
+        component.sendMessage();
+
+        expect(cnvSvcCreateConversationSpy.calls.count()).toEqual(1);
+        expect(cnvSvcCreateConversationSpy.calls.argsFor(0)).toEqual([convReq]);
+      });
+
+      it('should emit a conversationReady event', () => {
+        component.sendMessage();
+
+        expect(conversationReadyEmitSpy.calls.count()).toEqual(1);
+        expect(conversationReadyEmitSpy.calls.argsFor(0)).toEqual(['conversationid-1']);
+      });
+
+      it('should have set variables correctly', () => {
+        component.sendMessage();
+
+        expect(component.loading).toEqual(false);
+      });
+    });
   });
 
   describe('ngOnInit', () => {
