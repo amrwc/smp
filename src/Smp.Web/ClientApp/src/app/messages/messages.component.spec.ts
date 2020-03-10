e84a79c842160300e3d0782203d4bfef69b38133
faibz <louiswrwright@gmail.com>
2020-03-10 22:37:35 +0000

    #50 - Started adding tests for messages component.

--- src/Smp.Web/ClientApp/src/app/messages/messages.component.spec.ts
+++ src/Smp.Web/ClientApp/src/app/messages/messages.component.spec.ts
@@ -8,13 +8,54 @@ import { MessagesComponent } from './messages.component';
 import { MessagesService } from '../services/messages.service';
 import { ConversationComponent } from '../conversation/conversation.component';
 import { FormBuilder } from '@angular/forms';
+import { ExtendedConversation } from '../models/conversation';
+import { User } from '../models/user';
+import { FriendlyMessage } from '../models/message';
 
 describe('MessagesComponent', () => {
   let component: MessagesComponent;
   let fixture: ComponentFixture<MessagesComponent>;
 
+  const users = [
+    { id: 'userid-1', fullName: 'Jack Ryan', profilePictureUrl: 'website.com' } as User,
+    { id: 'userid-2', fullName: 'Tom Cruise', profilePictureUrl: 'website.co.uk' } as User,
+    { id: 'userid-3', fullName: 'Leonardo DiCaprio', profilePictureUrl: 'website.org' } as User
+  ];
+
+  const participantsOne = new Map<string, User>();
+  participantsOne.set(users[0].id, users[0]);
+  participantsOne.set(users[1].id, users[1]);
+
+  const participantsTwo = new Map<string, User>();
+  participantsOne.set(users[0].id, users[0]);
+  participantsTwo.set(users[2].id, users[2]);
+
+  const lastMessages = [
+    {
+      senderId: users[0].id,
+      sender: users[0]
+    } as FriendlyMessage,
+    {
+      senderId: users[2].id,
+      sender: users[2]
+    } as FriendlyMessage
+  ];
+
+  const conversations = [
+    { 
+      id: 'conversationid-1',
+      participants: participantsOne,
+      lastMessage: lastMessages[0]
+    } as ExtendedConversation,
+    { 
+      id: 'conversationid-2',
+      participants: participantsTwo,
+      lastMessage: lastMessages[1]
+    } as ExtendedConversation
+  ];
+
   beforeEach(() => {
-    localStorage.setItem('currentUser', '{ "id": "id" }');
+    localStorage.setItem('currentUser', '{ "id": "userid-1" }');
     TestBed.configureTestingModule({
       declarations: [ MessagesComponent, ConversationComponent ],
       imports: [HttpClientTestingModule],
@@ -38,7 +79,61 @@ describe('MessagesComponent', () => {
     localStorage.removeItem('currentUser');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+
+  });
+
+  describe('getConversationName', () => {
+    beforeEach(() => {
+      component.conversations = conversations;
+    });
+
+    it('should return the expected value', () => {
+      const conversationName = component.getConversationName('conversationid-1');
+      
+      expect(conversationName).toEqual('Tom Cruise');
+    });
+  });
+
+  describe('getConversationPicture', () => {
+    beforeEach(() => {
+      component.conversations = conversations;
+    });
+
+    it('should return the expected value', () => {
+      const conversationName = component.getConversationPicture('conversationid-1');
+      
+      expect(conversationName).toEqual('website.co.uk');
+    });
+  });
+
+  describe('getLastMessageSender', () => {
+    beforeEach(() => {
+      component.conversations = conversations;
+    });
+
+    it('should return the expected value', () => {
+      let lastMessageSender = component.getLastMessageSender('conversationid-1');
+      expect(lastMessageSender).toEqual('You');
+
+      lastMessageSender = component.getLastMessageSender('conversationid-2');
+      expect(lastMessageSender).toEqual('Leonardo DiCaprio');
+    });
+  });
+
+  describe('loadConversation', () => {
+
+  });
+
+  describe('showStartConversation', () => {
+    beforeEach(() => {
+      component.startNewConversation = false;
+    });
+
+    it('should set variables correctly', () => {
+      component.showStartConversation();
+
+      expect(component.startNewConversation).toEqual(true);
+    });
   });
 });
