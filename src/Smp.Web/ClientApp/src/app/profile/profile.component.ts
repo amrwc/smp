5bd9b359a63cdab76ee5564f336ac5b0b88f4085
amrwc <42574496+amrwc@users.noreply.github.com>
2020-03-13 21:01:53 +0000

    #50: Start adding tests for Profile component

--- src/Smp.Web/ClientApp/src/app/profile/profile.component.ts
+++ src/Smp.Web/ClientApp/src/app/profile/profile.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { User } from '../models/user';
-import { UsersService } from '../services/users.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { CreateRequestRequest } from '../models/requests/create-request-request';
+import { CurrentUser } from '../models/current-user';
 import { FeedComponent } from '../feed/feed.component';
 import { GlobalHelper } from '../helpers/global';
-import { CurrentUser } from '../models/current-user';
-import { RequestsService } from '../services/requests.service';
-import { CreateRequestRequest } from '../models/requests/create-request-request';
-import { RequestType } from '../models/request-type.enum';
-import { RelationshipsService } from '../services/relationships.service';
 import { RelationshipType } from '../models/relationship-type.enum';
+import { RelationshipsService } from '../services/relationships.service';
+import { RequestType } from '../models/request-type.enum';
+import { RequestsService } from '../services/requests.service';
+import { User } from '../models/user';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-profile',
@@ -36,7 +37,7 @@ export class ProfileComponent implements OnInit {
   ) {  }
 
   ngOnInit() {
-    let visitingUserId = this.globalHelper.localStorageItem<CurrentUser>('currentUser').id;
+    const visitingUserId = this.globalHelper.localStorageItem<CurrentUser>('currentUser').id;
     this.userId = this.route.snapshot.paramMap.get('id') ?? visitingUserId;
     this.showAddFriendButton = this.userId != visitingUserId;
 
@@ -52,6 +53,8 @@ export class ProfileComponent implements OnInit {
               next: () => {
                 this.showAddFriendButton = false;
                 this.friends = true;
+              },
+              error: (error: any) => {
               }
             });
           }
