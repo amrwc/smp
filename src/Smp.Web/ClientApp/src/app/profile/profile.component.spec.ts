b534e58407789f4f6c35a40ccf5a8b162a3b24e3
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2020-03-13 23:30:32 +0000

    #50: Add more tests to the Profile component

--- src/Smp.Web/ClientApp/src/app/profile/profile.component.spec.ts
+++ src/Smp.Web/ClientApp/src/app/profile/profile.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { throwError } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { GlobalHelper } from '../helpers/global';
 import { ProfileComponent } from './profile.component';
 import { RelationshipsService } from '../services/relationships.service';
@@ -48,29 +48,82 @@ describe('ProfileComponent', () => {
   });
 
   describe('ngOnInit', () => {
-    it("should show 'Add Friend' button", () => {
-      const requestsServiceGetRequestSpy = spyOn(TestBed.get(RequestsService), 'getRequest').and.returnValue(
-        throwError({ status: 404 })
-      );
-      const relationshipsServiceGetRelationshipSpy = spyOn(
-        TestBed.get(RelationshipsService),
-        'getRelationship'
-      ).and.returnValue(throwError(new Error()));
-      component.ngOnInit();
-      expect(component.showAddFriendButton).toBeTruthy();
-      expect(component.friends).toBeFalsy();
-      expect(requestsServiceGetRequestSpy.calls.count()).toEqual(1);
-      expect(requestsServiceGetRequestSpy.calls.argsFor(0)).toEqual([
-        JSON.parse(localStorage.getItem('currentUser')).id,
-        'bob',
-        RequestType.Friend,
-      ]);
-      expect(relationshipsServiceGetRelationshipSpy.calls.count()).toEqual(1);
-      expect(relationshipsServiceGetRelationshipSpy.calls.argsFor(0)).toEqual([
-        'bob',
-        JSON.parse(localStorage.getItem('currentUser')).id,
-        RequestType.Friend,
-      ]);
+    let requestsServiceGetRequestSpy: jasmine.Spy;
+    let relationshipsServiceGetRelationshipSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      requestsServiceGetRequestSpy = spyOn(TestBed.get(RequestsService), 'getRequest');
+      relationshipsServiceGetRelationshipSpy = spyOn(TestBed.get(RelationshipsService), 'getRelationship');
+    });
+
+    describe("should show 'Add Friend' button", () => {
+      afterEach(() => {
+        expect(component.showAddFriendButton).toBeTruthy();
+        expect(component.friends).toBeFalsy();
+        expect(component.requestPending).toBeFalsy();
+        expect(requestsServiceGetRequestSpy.calls.count()).toEqual(1);
+        expect(requestsServiceGetRequestSpy.calls.argsFor(0)).toEqual(getRequestsServiceGetRequestArgs());
+      });
+
+      it('when RequestsService.getRequest() returns error other than 404', () => {
+        requestsServiceGetRequestSpy.and.returnValue(throwError({ status: 400 }));
+        component.ngOnInit();
+      });
+
+      it('when RequestsService.getRequest() returns 404 error', () => {
+        requestsServiceGetRequestSpy.and.returnValue(throwError({ status: 404 }));
+        relationshipsServiceGetRelationshipSpy.and.returnValue(throwError(new Error()));
+        component.ngOnInit();
+        expect(relationshipsServiceGetRelationshipSpy.calls.count()).toEqual(1);
+        expect(relationshipsServiceGetRelationshipSpy.calls.argsFor(0)).toEqual(
+          getRelationshipsServiceGetRelationshipArgs()
+        );
+      });
+    });
+
+    describe("should not show 'Add Friend' button", () => {
+      afterEach(() => {
+        expect(component.showAddFriendButton).toBeFalsy();
+      });
+
+      it('when the user visits their own profile', () => {
+        localStorage.removeItem('currentUser');
+        localStorage.setItem('currentUser', '{ "id": "bob" }');
+        component.ngOnInit();
+      });
+
+      describe('when RequestsService.getRequest()', () => {
+        it('finds a pending friend request', () => {
+          requestsServiceGetRequestSpy.and.returnValue(of({}));
+          component.ngOnInit();
+          expect(component.friends).toBeFalsy();
+          expect(component.requestPending).toBeTruthy();
+          expect(requestsServiceGetRequestSpy.calls.count()).toEqual(1);
+          expect(requestsServiceGetRequestSpy.calls.argsFor(0)).toEqual(getRequestsServiceGetRequestArgs());
+        });
+
+        it('returns 404 error and RelationshipsService.getRelationship() finds a relationship', () => {
+          requestsServiceGetRequestSpy.and.returnValue(throwError({ status: 404 }));
+          relationshipsServiceGetRelationshipSpy.and.returnValue(of({}));
+          component.ngOnInit();
+          expect(component.friends).toBeTruthy();
+          expect(component.requestPending).toBeFalsy();
+          expect(requestsServiceGetRequestSpy.calls.count()).toEqual(1);
+          expect(requestsServiceGetRequestSpy.calls.argsFor(0)).toEqual(getRequestsServiceGetRequestArgs());
+          expect(relationshipsServiceGetRelationshipSpy.calls.count()).toEqual(1);
+          expect(relationshipsServiceGetRelationshipSpy.calls.argsFor(0)).toEqual(
+            getRelationshipsServiceGetRelationshipArgs()
+          );
+        });
+      });
     });
   });
 });
+
+function getRequestsServiceGetRequestArgs(): Array<any> {
+  return [JSON.parse(localStorage.getItem('currentUser')).id, 'bob', RequestType.Friend];
+}
+
+function getRelationshipsServiceGetRelationshipArgs(): Array<any> {
+  return ['bob', JSON.parse(localStorage.getItem('currentUser')).id, RequestType.Friend];
+}
