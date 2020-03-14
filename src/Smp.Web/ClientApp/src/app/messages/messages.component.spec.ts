51d45a88c59168a2bfcec1687ae376fb4214e429
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2020-03-14 21:11:59 +0000

    #50: Remove redundant providers and imports from Message component

--- src/Smp.Web/ClientApp/src/app/messages/messages.component.spec.ts
+++ src/Smp.Web/ClientApp/src/app/messages/messages.component.spec.ts
@@ -3,7 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ConversationsService } from '../services/conversations.service';
 import { UsersService } from '../services/users.service';
-import { GlobalHelper } from '../helpers/global';
 import { MessagesComponent } from './messages.component';
 import { MessagesService } from '../services/messages.service';
 import { ConversationComponent } from '../conversation/conversation.component';
@@ -67,10 +66,6 @@ describe('MessagesComponent', () => {
       declarations: [ MessagesComponent, ConversationComponent ],
       imports: [HttpClientTestingModule],
       providers: [
-        GlobalHelper,
-        ConversationsService,
-        MessagesService,
-        UsersService,
         FormBuilder,
         { provide: 'BASE_URL', useValue: "https://www.smp.org/" }
       ]
