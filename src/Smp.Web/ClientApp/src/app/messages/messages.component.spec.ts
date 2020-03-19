53a1d0ca15d209742c8301dccd41724bf0a5ab14
amrwc <42574496+amrwc@users.noreply.github.com>
2020-03-19 23:34:34 +0000

    #50: Refactor MessagesComponent's unit tests
    TODO:
    - add tests for the ngOnInit() lifecycle method.

--- src/Smp.Web/ClientApp/src/app/messages/messages.component.spec.ts
+++ src/Smp.Web/ClientApp/src/app/messages/messages.component.spec.ts
@@ -1,77 +1,63 @@
+import { FormBuilder } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { of } from 'rxjs';
+import { ConversationComponent } from '../conversation/conversation.component';
 import { ConversationsService } from '../services/conversations.service';
-import { UsersService } from '../services/users.service';
+import { ExtendedConversation } from '../models/conversation';
+import { FriendlyMessage } from '../models/message';
 import { MessagesComponent } from './messages.component';
 import { MessagesService } from '../services/messages.service';
-import { ConversationComponent } from '../conversation/conversation.component';
-import { FormBuilder } from '@angular/forms';
-import { ExtendedConversation } from '../models/conversation';
 import { User } from '../models/user';
-import { FriendlyMessage } from '../models/message';
-import { of } from 'rxjs';
+import { UsersService } from '../services/users.service';
 
 describe('MessagesComponent', () => {
+  const participantsOne = new Map<string, User>();
+  const participantsTwo = new Map<string, User>();
+  const users: User[] = [
+    { id: 'userid-1', fullName: 'Jack Ryan', profilePictureUrl: 'website.com' },
+    { id: 'userid-2', fullName: 'Tom Cruise', profilePictureUrl: 'website.co.uk' },
+    { id: 'userid-3', fullName: 'Leonardo DiCaprio', profilePictureUrl: 'website.org' },
+  ] as User[];
+  const lastMessages: FriendlyMessage[] = [
+    { senderId: users[0].id, sender: users[0] },
+    { senderId: users[2].id, sender: users[2] },
+  ] as FriendlyMessage[];
+  const conversations: ExtendedConversation[] = [
+    {
+      id: 'conversationid-1',
+      participants: participantsOne,
+      lastMessage: lastMessages[0],
+    },
+    {
+      id: 'conversationid-2',
+      participants: participantsTwo,
+      lastMessage: lastMessages[1],
+    },
+  ] as ExtendedConversation[];
+  const unloadedConversation: ExtendedConversation = {
+    id: 'conversationid-3',
+    participants: participantsOne,
+    lastMessage: lastMessages[0],
+  } as ExtendedConversation;
   let component: MessagesComponent;
   let fixture: ComponentFixture<MessagesComponent>;
 
-  const users = [
-    { id: 'userid-1', fullName: 'Jack Ryan', profilePictureUrl: 'website.com' } as User,
-    { id: 'userid-2', fullName: 'Tom Cruise', profilePictureUrl: 'website.co.uk' } as User,
-    { id: 'userid-3', fullName: 'Leonardo DiCaprio', profilePictureUrl: 'website.org' } as User
-  ];
-
-  const participantsOne = new Map<string, User>();
-  participantsOne.set(users[0].id, users[0]);
-  participantsOne.set(users[1].id, users[1]);
-
-  const participantsTwo = new Map<string, User>();
-  participantsOne.set(users[0].id, users[0]);
-  participantsTwo.set(users[2].id, users[2]);
-
-  const lastMessages = [
-    {
-      senderId: users[0].id,
-      sender: users[0]
-    } as FriendlyMessage,
-    {
-      senderId: users[2].id,
-      sender: users[2]
-    } as FriendlyMessage
-  ];
-
-  const conversations = [
-    { 
-      id: 'conversationid-1',
-      participants: participantsOne,
-      lastMessage: lastMessages[0]
-    } as ExtendedConversation,
-    { 
-      id: 'conversationid-2',
-      participants: participantsTwo,
-      lastMessage: lastMessages[1]
-    } as ExtendedConversation
-  ];
-
-  const unloadedConversation = { 
-      id: 'conversationid-3',
-      participants: participantsOne,
-      lastMessage: lastMessages[0]
-    } as ExtendedConversation;
+  beforeAll(() => {
+    participantsOne.set(users[0].id, users[0]);
+    participantsOne.set(users[1].id, users[1]);
+    participantsOne.set(users[0].id, users[0]);
+    participantsTwo.set(users[2].id, users[2]);
+  });
 
   beforeEach(() => {
     localStorage.setItem('currentUser', JSON.stringify({ id: users[0].id }));
     TestBed.configureTestingModule({
-      declarations: [ MessagesComponent, ConversationComponent ],
+      declarations: [MessagesComponent, ConversationComponent],
       imports: [HttpClientTestingModule],
-      providers: [
-        FormBuilder,
-        { provide: 'BASE_URL', useValue: "https://www.smp.org/" }
-      ]
-    })
-    .compileComponents();
-
+      providers: [FormBuilder, { provide: 'BASE_URL', useValue: 'https://www.smp.org/' }],
+    }).compileComponents();
     fixture = TestBed.createComponent(MessagesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -81,151 +67,109 @@ describe('MessagesComponent', () => {
     localStorage.removeItem('currentUser');
   });
 
-  describe('ngOnInit', () => {
+  describe('ngOnInit()', () => {});
 
-  });
-
-  describe('getConversationName', () => {
-    beforeEach(() => {
+  describe('getConversationName()', () => {
+    it('should have returned the expected value', () => {
       component.conversations = conversations;
-    });
-
-    it('should return the expected value', () => {
       const conversationName = component.getConversationName('conversationid-1');
-      
       expect(conversationName).toEqual('Tom Cruise');
     });
   });
 
-  describe('getConversationPicture', () => {
-    beforeEach(() => {
+  describe('getConversationPicture()', () => {
+    it('should have returned the expected value', () => {
       component.conversations = conversations;
-    });
-
-    it('should return the expected value', () => {
       const conversationName = component.getConversationPicture('conversationid-1');
-      
       expect(conversationName).toEqual('website.co.uk');
     });
   });
 
-  describe('getLastMessageSender', () => {
-    beforeEach(() => {
+  describe('getLastMessageSender()', () => {
+    it('should have returned the expected value', () => {
       component.conversations = conversations;
-    });
-
-    it('should return the expected value', () => {
       let lastMessageSender = component.getLastMessageSender('conversationid-1');
       expect(lastMessageSender).toEqual('You');
-
       lastMessageSender = component.getLastMessageSender('conversationid-2');
       expect(lastMessageSender).toEqual('Leonardo DiCaprio');
     });
   });
 
-  describe('loadConversation', () => {
-    let cnvSvcGetConversationsSpy: jasmine.Spy;
-
+  describe('loadConversation()', () => {
     beforeEach(() => {
-      cnvSvcGetConversationsSpy = spyOn(TestBed.inject(ConversationsService), 'getConversations');
       component.conversations = conversations;
-
       let cnvId = '';
-
-      const setterSpy = spyOnProperty(component.conversation, 'conversationId', 'set')
-        .and.callFake((convId: string) => {
-          cnvId = convId;
+      spyOnProperty(component.conversation, 'conversationId', 'set').and.callFake((convId: string) => {
+        cnvId = convId;
       });
-
-      const getterSpy = spyOnProperty(component.conversation, 'conversationId', 'get')
-        .and.callFake(() => {
-          return cnvId;
+      spyOnProperty(component.conversation, 'conversationId', 'get').and.callFake(() => {
+        return cnvId;
       });
     });
 
     describe('if the conversation has not already been loaded', () => {
-      let cnvSvcGetConversationParticipantsSpy: jasmine.Spy;
-      let usrSvcGetUserSpy: jasmine.Spy;
-      let msgSvcGetMessagesFromConversationSpy: jasmine.Spy;
-
       const convId = unloadedConversation.id;
 
       beforeEach(() => {
-        cnvSvcGetConversationsSpy.and.returnValue(of([unloadedConversation]));
-        cnvSvcGetConversationParticipantsSpy = spyOn(TestBed.inject(ConversationsService), 'getConversationParticipants')
-          .and.returnValue(of([users[0].id, users[1].id]));
-        usrSvcGetUserSpy = spyOn(TestBed.inject(UsersService), 'getUser')
-          .and.returnValue(of(users[0]));
-        msgSvcGetMessagesFromConversationSpy = spyOn(TestBed.inject(MessagesService), 'getMessagesFromConversation')
-          .and.returnValue(of(lastMessages[0]));
-
+        spyOn(TestBed.inject(ConversationsService), 'getConversations').and.returnValue(of([unloadedConversation]));
+        spyOn(TestBed.inject(ConversationsService), 'getConversationParticipants').and.returnValue(
+          of([users[0].id, users[1].id])
+        );
+        spyOn(TestBed.inject(UsersService), 'getUser').and.returnValue(of(users[0]));
+        spyOn(TestBed.inject(MessagesService), 'getMessagesFromConversation').and.returnValue(of(lastMessages[0]));
         component.conversation.conversationId = unloadedConversation.id;
+        component.loadConversation(convId);
       });
 
       it('should set variables correctly', () => {
-        component.loadConversation(convId);
-
         expect(component.startNewConversation).toEqual(false);
         expect(component.conversation.conversationId).toEqual(convId);
         expect(component.conversations).toEqual([unloadedConversation]);
       });
 
-      it('should call ConversationsService getConversations',() => {
-        component.loadConversation(convId);
-
-        expect(cnvSvcGetConversationsSpy.calls.count()).toEqual(1);
-        expect(cnvSvcGetConversationsSpy.calls.argsFor(0)).toEqual([users[0].id]);
+      it('should call ConversationsService.getConversations()', () => {
+        expect(TestBed.get(ConversationsService).getConversations).toHaveBeenCalledTimes(1);
+        expect(TestBed.get(ConversationsService).getConversations).toHaveBeenCalledWith(users[0].id);
       });
 
-      it('should call ConversationsService getConversationParticipants', () => {
-        component.loadConversation(convId);
-
-        expect(cnvSvcGetConversationParticipantsSpy.calls.count()).toEqual(1);
-        expect(cnvSvcGetConversationParticipantsSpy.calls.argsFor(0)).toEqual([unloadedConversation.id]);
+      it('should call ConversationsService.getConversationParticipants()', () => {
+        expect(TestBed.get(ConversationsService).getConversationParticipants).toHaveBeenCalledTimes(1);
+        expect(TestBed.get(ConversationsService).getConversationParticipants).toHaveBeenCalledWith(
+          unloadedConversation.id
+        );
       });
 
-      it('should call UsersService getUser', () => {
-        component.loadConversation(convId);
-
-        expect(usrSvcGetUserSpy.calls.count()).toEqual(2);
-        expect(usrSvcGetUserSpy.calls.argsFor(0)).toEqual([users[0].id]);
-        expect(usrSvcGetUserSpy.calls.argsFor(1)).toEqual([users[1].id]);
+      it('should call UsersService.getUser()', () => {
+        expect(TestBed.get(UsersService).getUser).toHaveBeenCalledTimes(2);
+        expect(TestBed.get(UsersService).getUser).toHaveBeenCalledWith(users[0].id);
+        expect(TestBed.get(UsersService).getUser).toHaveBeenCalledWith(users[1].id);
       });
 
-      it('should call MessagesService getMessagesFromConversation', () => {
-        component.loadConversation(convId);
-
-        expect(msgSvcGetMessagesFromConversationSpy.calls.count()).toEqual(1);
-        expect(msgSvcGetMessagesFromConversationSpy.calls.argsFor(0)).toEqual([
-          convId, 1, 0
-        ]);
+      it('should call MessagesService.getMessagesFromConversation()', () => {
+        expect(TestBed.get(MessagesService).getMessagesFromConversation).toHaveBeenCalledTimes(1);
+        expect(TestBed.get(MessagesService).getMessagesFromConversation).toHaveBeenCalledWith(convId, 1, 0);
       });
     });
 
     describe('if the conversation has already been loaded', () => {
       it('should set variables correctly', () => {
         component.loadConversation(conversations[0].id);
-
         expect(component.startNewConversation).toEqual(false);
         expect(component.conversation.conversationId).toEqual(conversations[0].id);
       });
 
-      it('should not call ConversationsService getConversations', () => {
+      it('should not call ConversationsService.getConversations()', () => {
+        spyOn(TestBed.inject(ConversationsService), 'getConversations');
         component.loadConversation(conversations[0].id);
-
-        expect(cnvSvcGetConversationsSpy.calls.count()).toEqual(0);
+        expect(TestBed.get(ConversationsService).getConversations).toHaveBeenCalledTimes(0);
       });
     });
   });
 
-  describe('showStartConversation', () => {
-    beforeEach(() => {
-      component.startNewConversation = false;
-    });
-
+  describe('showStartConversation()', () => {
     it('should set variables correctly', () => {
+      component.startNewConversation = false;
       component.showStartConversation();
-
       expect(component.startNewConversation).toEqual(true);
     });
   });
