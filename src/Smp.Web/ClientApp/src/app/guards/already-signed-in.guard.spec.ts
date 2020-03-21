5b44ceec998a5261172a8272bb9a8ad0f6f1817c
faibz <louiswrwright@gmail.com>
2020-03-21 00:38:34 +0000

    #50 - Refactored guards tests.

--- src/Smp.Web/ClientApp/src/app/guards/already-signed-in.guard.spec.ts
+++ src/Smp.Web/ClientApp/src/app/guards/already-signed-in.guard.spec.ts
@@ -9,21 +9,17 @@ describe('AlreadySignedInGuard canActivate', () => {
   const router: Router = new MockRouter() as Router;
   const alreadySignedInGuard = new AlreadySignedInGuard(router);
 
-  beforeAll(() => {
-    localStorage.removeItem('currentUser');
-  });
+  describe('canActivate()', () => {
+    it('should have returned true for a non-logged in user', () => {
+      expect(alreadySignedInGuard.canActivate()).toEqual(true);
+    });
+  
+    it ('should have returned false for a logged in user', () => {
+      localStorage.setItem('currentUser', 'currentUser');
+  
+      expect(alreadySignedInGuard.canActivate()).toEqual(false);
 
-  afterEach(() => {
-    localStorage.removeItem('currentUser');
-  });
-
-  it('should return true for a non-logged in user', () => {
-    expect(alreadySignedInGuard.canActivate()).toEqual(true);
-  });
-
-  it ('should return false for a logged in user', () => {
-    localStorage.setItem('currentUser', 'currentUser');
-
-    expect(alreadySignedInGuard.canActivate()).toEqual(false);
+      localStorage.removeItem('currentUser');
+    });
   });
 });
--- src/Smp.Web/ClientApp/src/app/guards/auth.guard.spec.ts
+++ src/Smp.Web/ClientApp/src/app/guards/auth.guard.spec.ts
@@ -6,21 +6,21 @@ class MockRouter {
 }
 
 describe('AuthGuard canActivate', () => {
-  afterEach(() => {
-    localStorage.removeItem('currentUser');
-  })
-
   const router: Router = new MockRouter() as Router;
   const authGuard = new AuthGuard(router);
   let state = { url: '/' } as RouterStateSnapshot;
 
-  it('should return true for a logged in user', () => {
-    localStorage.setItem('currentUser', 'currentUser');
+  describe('canActivate()', () => {
+    it('should have returned true for a logged in user', () => {
+      localStorage.setItem('currentUser', 'currentUser');
+  
+      expect(authGuard.canActivate(null, state)).toEqual(true);
 
-    expect(authGuard.canActivate(null, state)).toEqual(true);
-  });
-
-  it ('should return false for a non-logged in user', () => {
-    expect(authGuard.canActivate(null, state)).toEqual(false);
+      localStorage.removeItem('currentUser');
+    });
+  
+    it ('should have returned false for a non-logged in user', () => {
+      expect(authGuard.canActivate(null, state)).toEqual(false);
+    });
   });
 });
